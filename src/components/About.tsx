61462c93af29072aa4c123a2574533fd8a3af550
imanelawadi90-glitch <imanelawadi90@gmail.com>
2025-09-19 00:03:14 +0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -44,7 +44,8 @@ const About = () => {
             </p>
           </div>
 
-
+          {/* About Content */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
 
          
             {/* Skills */}
