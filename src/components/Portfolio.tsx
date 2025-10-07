e7cffd0a7a56479e7191e3372aa0b0f5ce710580
imanelawadi90-glitch <imanelawadi90@gmail.com>
2025-10-07 16:06:22 +0300

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -285,7 +285,8 @@ const Portfolio = () => {
           <div className="w-20 h-1 accent-gradient mx-auto mb-6"></div>
           <p className="text-lg text-muted-foreground max-w-2xl mx-auto mb-8">
           Showcasing My Creative Video Projects </p>
-             <p>For Additional Work: More links available upon request.
+             <p className="text-lg text-muted-foreground max-w-2xl mx-auto mb-8">
+               For Additional Work: More links available upon request.
           </p>
         </div>
 
